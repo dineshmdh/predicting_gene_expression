bbb09db770e1c51a9f237a6e1206dfee323ebfba
Dinesh Manandhar <didesh99@gmail.com>
2018-01-27 15:22:24 -0500

    Changed the parameter search function so that lower limit < upper limit. Note, lower limit cannot equal upper limit.

--- Functions/model_scripts/helper_scripts/HPO_helper.py
+++ Functions/model_scripts/helper_scripts/HPO_helper.py
@@ -3,8 +3,11 @@ import hyperopt
 from functools import partial
 from hyperopt import hp
 
-'''Define some basic HPO functions and variables'''
-LAMDA_BASE = 3  # used to define lamda in parameter space search
+'''Define some basic HPO functions and variables
+LAMDA_LOWER (below) should be > LAMDA_UPPER because of this:
+https://github.com/hyperopt/hyperopt/issues/221
+'''
+LAMDA_BASE = 2  # used to define lamda in parameter space search
 LAMDA_LOWER = 1  # lower limit in 10e-{} space to select lamda
 LAMDA_UPPER = 7
 
@@ -28,19 +31,19 @@ tpe_method = partial(
 
 
 def get_parameter_space_forHPO(trainX):
-    h1_lower = int(0.33 * trainX.shape[1])
-    h1_upper = int(0.66 * trainX.shape[1])
+    h1_lower = int(round(0.33 * trainX.shape[1]))  # if just 4 fts, h1_lower = 1 (see below)
+    h1_upper = int(round(0.66 * trainX.shape[1]))  # even if just 4 fts, h1_upper = 3
     parameter_space = {
         'layers': hp.choice('layers', [{
             'n_layers': 1,
             'n_units_layer': [
-                uniform_int('n_units_layer_11', h1_lower, h1_upper),
+                uniform_int('n_units_layer_11', max(2, h1_lower), h1_upper),  # don't allow lower lim to be 1
             ],
         }, {
             'n_layers': 2,
             'n_units_layer': [
-                uniform_int('n_units_layer_21', max(2, h1_lower), max(2, h1_upper)),
-                uniform_int('n_units_layer_22', max(1, int(0.66 * h1_lower)), max(int(0.66 * h1_upper), 1)),
+                uniform_int('n_units_layer_21', max(2, h1_lower), h1_upper),  # don't allow lower lim to be 1
+                uniform_int('n_units_layer_22', max(2, int(round(0.66 * h1_lower))), max(3, int(round(0.66 * h1_upper)))),  # h2 layer will have 2-3 nodes; doing max(3,_) b/c if for eg. h1_upper = 3, upper limit has to be > lower limit
             ],
         }]),
         'lamda': LAMDA_BASE * 10**(-1 * uniform_int("lamda", LAMDA_LOWER, LAMDA_UPPER))
